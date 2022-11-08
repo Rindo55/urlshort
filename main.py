12c9bd819ddeee7365dd7ecdeac5b25f54f1459f
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-11-08 15:41:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -85,13 +85,13 @@ async def upload(client, message):
         callapi = requests.post("https://api.filechan.org/upload", files=files)
         text = callapi.json()
         long_url = text['data']['file']['url']['full']
-        api_url = f"https://tnlink.in/api?api=fea911843f6e7bec739708f3e562b56184342089&url={long_url}&alias=fuk567356&format=text"
+        api_url = f"https://tnlink.in/api?api=fea911843f6e7bec739708f3e562b56184342089&url={long_url}&alias=CustomAlias&format=text"
         result = requests.get(api_url)
         nai_text = result.text
         output = f"""
 {file_caption}
 ━━━━━━━━━━━━━━━━━━━
-{nai_text}"""
+[🔗Download Link]{nai_text}"""
         daze = await m.edit(output)
     except Exception:
        await OC_AnonFilesBot.send_message(message.chat.id, text="Something Went Wrong!")
