e83bff1f7f865beda0ea6e6b271804cef246b39e
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-11-08 12:35:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    BOT_TOKEN = "5581555255:AAGAit8r0QKEXC_DmQ62Y8VtbMZQPPvLinQ"
+    BOT_TOKEN = "5786017840:AAEsbeA-1QUdr_0Stp3Bg8V0ov0kxl1A_28"
 
     APP_ID = "10247139"
 
