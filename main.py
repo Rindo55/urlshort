f6e5c24c7d63457fc043ad6a2586bd1630d13ed4
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-11-08 15:22:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -84,8 +84,8 @@ async def upload(client, message):
         await m.edit(file_caption + "\n" "━━━━━━━━━━━━━━━━━━━" + "\n" + "`Generating Link`**")
         callapi = requests.post("https://api.filechan.org/upload", files=files)
         text = callapi.json()
-        fil_text = text['data']['file']['url']['full']
-        long_url = urlencode(fil_text)
+        fil_url = text['data']['file']['url']['full']
+        long_url = urlencode('{fil_url}')
         api_url = "https://tnlink.in/api?api=fea911843f6e7bec739708f3e562b56184342089&url={long_url}&alias=fuk567356&format=text"
         result = requests.get(api_url)
         nai_text = result.text
