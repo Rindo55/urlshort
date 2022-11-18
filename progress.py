98365f0602d3ad5ea02d35d0fc67d7d4d3bf94ad
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2022-11-18 10:43:32 +0530

    Update progress.py

--- progress.py
+++ progress.py
@@ -24,10 +24,9 @@ def time_data(start_time):
 async def progress(current, total,up_msg, message, start_time):
 
     try:
-        asyncio.sleep(3)
         await message.edit(
             text = f"{up_msg} {current * 100 / total:.1f}% in {time_data(start_time)}"
                 )
-
+        await asyncio_sleep(5)
     except:
         pass
