f98ca7083ae71d2f54c5b5343b70904731ef373d
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-03-25 00:36:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ async def upload(client, message):
         output = f"""
 ━━━━━━━━━━━━━━━━━━━
 **External Download Links**
-{gofinal}"""
+{go_text}"""
         daze = await m.edit(output, parse_mode = "markdown")
     except Exception:
        await OC_AnonFilesBot.send_message(message.chat.id, text="Something Went Wrong!")
